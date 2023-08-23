bcc85249e750687c634b1dd9b426bec716ab913c
master <mehdi.elomer@ayssoft.com>
2023-08-23 14:28:58 +0300

    Initial Commit

---
+++ SwiftuiMapApp/SwiftuiMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiMapAppApp.swift
+//  SwiftuiMapApp
+//
+//  Created by NurullahMac on 23.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
