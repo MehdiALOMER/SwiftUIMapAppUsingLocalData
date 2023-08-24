a165e8cb1568da690def9bbe342acefc7bd8c805
master <mehdi.elomer@ayssoft.com>
2023-08-25 00:26:57 +0300

    add location preview, map annotation and detail views

--- SwiftuiMapApp/ViewModels/LocationsViewModel.swift
+++ SwiftuiMapApp/ViewModels/LocationsViewModel.swift
@@ -28,6 +28,9 @@ class LocationsViewModel: ObservableObject {
     // Show list of locations
     @Published var showLocationsList: Bool = false
     
+    // Show location detail via sheet
+    @Published var sheetLocation: Location? = nil
+    
     init() {
         let locations = LocationsData.locations
         self.locations = locations
@@ -56,5 +59,27 @@ class LocationsViewModel: ObservableObject {
             mapLocation = location
             showLocationsList = false
         }
+    }
+    
+    func nextButtonPressed() {
+        // Get the current index
+        guard let currentIndex = locations.firstIndex(where: { $0 == mapLocation }) else {
+            print("Could not find current index in locations array! Should never happen.")
+            return
+        }
+        
+        // Check if the currentIndex is valid
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            // Next Index in NOT valid
+            // Restart from 0
+            guard let firstLocation = locations.first else { return }
+            showNextLocation(location: firstLocation)
+            return
+        }
+        
+        // Next Index IS valid
+        let nextLocation = locations[nextIndex]
+        showNextLocation(location: nextLocation)
     }
 }
